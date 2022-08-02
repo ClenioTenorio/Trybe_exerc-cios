e7adad03fa21a62ccb30255388517ae973a2b4b0
Clenio Tenorio <cleniotenorio@gmail.com>
2022-08-02 16:58:23 -0300

    Arquivos Modificados de outros diretorios

---
+++ Bloco_8/Exercicios/Lições_8.1.js
@@ -0,0 +1,25 @@
+// let meuTexto = 'Acordado!!'
+// let novaString = meuTexto.replace('string', 'sauge');
+// console.log(novaString);
+
+// Crie uma função que retorne a string 'Acordando!!';
+const jose = () => 'Acordando!!';
+// Crie outra função que retorne a string 'Bora tomar café!!';
+const clenio = () => 'Bora tomar café!!';
+// Crie mais uma função que retorne a string 'Partiu dormir!!';
+const tenorio = () => 'Partiu dormir!!';
+// Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores. Exemplo:
+// Ao chamar a função doingThings:
+// doingThings(wakeUp);
+
+// Ela deve retornar o valor do respectivo parâmetro, neste caso:
+// Acordando!!
+
+const doingThings = (callback) => {
+    const result = callback();
+    console.log(result);
+};
+
+doingThings(jose);
+doingThings(clenio);
+doingThings(tenorio);
